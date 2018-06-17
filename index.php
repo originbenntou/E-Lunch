40c1a7d06ab750d2933117a5bfecf56cb3f3e326
originbenntou8973 <originbenntou8973@gmail.com>
2018-06-18 02:25:24 +0900

    近隣の店検索機能追加

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
 
-require_once(dirname(__FILE__) . '/json.php');
+require_once(dirname(__FILE__) . '/php/DecideTheDestiney.php');
 
-$store = (new ELLunchList())->getNormalList();
-
-$rand = mt_rand() / mt_getrandmax();
-
-function decideTheDestiny($store, $rand) {
-	static $probability = 0;
-	foreach ($store as $name => $value) {
-		$probability += $value;
-		if ($rand <= $probability) {
-			return $name;
-		}
-	}
-	throw new Exception("管理者に報告してください");
-}
 ?>
 
 <!DOCTYPE html>
@@ -25,12 +11,6 @@ function decideTheDestiny($store, $rand) {
 	<title>E-Lunch</title>
 	<link rel="stylesheet" href="css/style.css">
 	<!-- <script type="text/javascript" src="js/main.js"></script> -->
-	<style>
-       #map {
-        height: 400px;
-        width: 100%;
-       }
-    </style>
 </head>
 <body>
 	<header>E-Lunch</header>
@@ -49,7 +29,7 @@ function decideTheDestiny($store, $rand) {
 		<button onclick="window.location.reload();">再試行</button>
 	</div>
 	<div class="discover">
-		<button onclick="location.replace('/search.php');">近場のランチ</button>
+		<button onclick="location.replace('/search.html');">近場のランチ</button>
 	</div>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,31 +1,94 @@
-function success(position) {
-    var latitude = position.coords.latitude,
-     longitude = position.coords.longitude;
+let searched = [];
 
-    const request = new XMLHttpRequest(),
-     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
-     radius = 1000,
-     type = "restaurant",
-     apiKey = "AIzaSyDI8KywRZhloYIrEDGvxXe-3Je1uEGi5FI";
+// TODO: MAP初期化なしで動作するように修正
+function initMap() {
+    let options = {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+    };
+    
+    navigator.geolocation.getCurrentPosition(success, error, options);
 
-    request.open("GET", "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.8639021,130.8981866&radius=50000&type=restaurant&key=AIzaSyDI8KywRZhloYIrEDGvxXe-3Je1uEGi5FI");
-    request.addEventListener("load", (event) => {
-        console.log("dgdaga");
-        console.log(event);
-    });
-    request.send();
-};
+    function success(position) {
+        let pyrmont = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-function error(err) {
-    alert("位置情報を取得できません");
-    console.log(err);
-};
+        let map = new google.maps.Map(document.getElementById('map'), {
+            center: pyrmont,
+            zoom: 15
+        });
 
+        let service = new google.maps.places.PlacesService(map);
 
-var options = {
-    enableHighAccuracy: true,
-    timeout: 500,
-    maximumAge: 0
-};
+        let request = {
+            location: pyrmont,
+            radius: '500',
+            types: ['restaurant']
+        };
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+        service.nearbySearch(request, callback);
+        
+        function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                console.log(results);
+                for (let i = 0; i < results.length; i++) {
+                    searched[i] = results[i].name;
+                }
+                $.ajax({
+                    type: "POST",
+                    url: "php/DecideTheDestiney.php",
+                    data: {
+                        "store": searched
+                    }
+                }).done(function (res, text, xhr) {
+                    let count = 0;
+                    let delay = 100;
+                    let interval, interval2, interval3, interval4, interval5;
+
+                    let roulette = function() {
+                        moment = searched[Math.floor(Math.random() * searched.length)];
+
+                        let resultObj = $(".result");
+                        resultObj.html(moment);
+                        count++;
+                        if (count === 20) {
+                            clearTimeout(interval);
+                            interval2 = setInterval(roulette, 200);
+                        }
+
+                        if (count === 27) {
+                            clearTimeout(interval2);
+                            interval3 = setInterval(roulette, 800);
+                        }
+
+                        if (count === 29) {
+                            clearTimeout(interval3);
+                            interval4 = setInterval(roulette, 1200);
+                        }
+
+                        if (count === 31) {
+                            clearTimeout(interval4);
+                            interval5 = setInterval(roulette, 1800);
+                        }
+
+                        if (count === 32) {
+                            clearTimeout(interval5);
+                            $(".decision").removeClass("hidden");
+                            $(".reload").addClass("hidden");
+                            // TODO: DirectionAPIで再実装
+                            resultObj.html(`<a target="_blank" href=https://www.google.co.jp/search?q="` + encodeURIComponent(xhr.responseText) + `">${xhr.responseText}</a>`);
+                        }
+                    };
+                    // ルーレットスタート
+                    interval = setInterval(roulette, delay);
+                }).fail(function () {
+                    //TODO: エラー処理
+                });
+            }
+        }        
+    }
+
+    function error() {
+        alert("位置情報が取得できません");
+    }
+}
