00ea83956b64b2d18f58359d1578c7676341bb09
originbenntou8973 <originbenntou8973@gmail.com>
2018-06-19 23:16:48 +0900

    .htaccess追加

--- index.php
+++ index.php
@@ -1,36 +1,21 @@
-<?php
-
-require_once(dirname(__FILE__) . '/php/DecideTheDestiney.php');
-
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
 	<title>E-Lunch</title>
 	<link rel="stylesheet" href="css/style.css">
-	<!-- <script type="text/javascript" src="js/main.js"></script> -->
 </head>
 <body>
 	<header>E-Lunch</header>
-	<div class="text">今日の餌</div>
-	<img src="./img/osara.png" alt="餌">
-	<div class="result">
-		<?php
-			try {
-				echo decideTheDestiny($store, $rand), PHP_EOL;
-			} catch (Exception $e) {
-				echo $e, PHP_EOL;;
-			}
-		?>
+	<div class="text">ガチャガチャを選択してください</div>
+	<div class="button">
+		<button onclick="location.replace('/normal');">ノーマルガチャ</button>
 	</div>
-	<div class="reload">
-		<button onclick="window.location.reload();">再試行</button>
+	<div class="button">
+		<button onclick="location.replace('/secret');">シークレットガチャ<br>(メンテナンス中)</button>
 	</div>
-	<div class="discover">
-		<!-- SSL化 -->
-		<button onclick="location.replace('https://13.115.145.47/search.html');">近場のランチ</button>
+	<div class="button">
+		<button onclick="location.replace('/pickup');">ピックアップガチャ<br>(メンテナンス中)</button>
 	</div>
 </body>
 </html>
---
+++ normal.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once(dirname(__FILE__) . '/php/DecideTheDestiney.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>E-Lunch ノーマルガチャ</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>E-Lunch</header>
+<div class="text">今日の餌</div>
+<img src="./img/osara.png" alt="餌">
+<div class="result">
+	<?php
+	try {
+		echo decideTheDestiny($store, $rand), PHP_EOL;
+	} catch (Exception $e) {
+		echo $e, PHP_EOL;;
+	}
+	?>
+</div>
+<div class="reload">
+	<button onclick="window.location.reload();">再試行</button>
+</div>
+<div class="back">
+	<button onclick="location.replace('/index');">戻る</button>
+</div>
+</body>
+</html>
