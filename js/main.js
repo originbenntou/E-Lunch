ecde1d5b8087624903f3e586241e1d0ec300df25
originbenntou8973 <originbenntou8973@gmail.com>
2018-06-30 11:43:09 +0900

    エラーページ追加

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function initMap() {
         };
 
         service.nearbySearch(request, callback);
-        
+
         function callback(results, status) {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
                 for (let i = 0; i < results.length; i++) {
