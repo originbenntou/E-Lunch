682e5a3967db46133a58995a462ad8fb09030d0e
originbenntou8973 <originbenntou8973@gmail.com>
2018-07-17 23:29:49 +0900

    jQueryがコンパイルできるようになった

--- src/components/Gacha.vue
+++ src/components/Gacha.vue
@@ -17,76 +17,54 @@
         console.log( 'jquery', $.fn.jquery );
     }());
 
-    import DecideTheDestiney from '../js/decideTheDestiney.js';
-    import Roulette from '../js/roulette.js';
+    function _getShopJson(): any {
+        return new Promise(function (resolve: any) {
+            $.getJSON('./data/lunch.json', function (shops: any) {
+                // 等確率追加
+                shops.forEach(function (obj: any) {
+                    obj.ratio = 1 / shops.length;
+                });
 
-    (function() {
-        console.log(DecideTheDestiney, Roulette);
-    }());
+                resolve(shops);
+            });
+        });
+    }
+
+    function doGacha(): any {
+        return new Promise(resolve => {
+            let rand = Math.random();
+
+            _getShopJson().then(function (shops: any) {
+                let probability = 0;
+                for (let i: number = 0; i < shops.length; i++) {
+                    probability += shops[i].ratio;
+
+                    if (rand <= probability) {
+                        resolve([shops, shops[i].name]);
+                    }
+                }
+            });
+        });
+    }
 
     export default Vue.extend({
         data() {
+            var list: any,
+                result: any;
             return {
-                aaa: "aaa",
+                list,
+                result
             }
         },
         methods: {
-            start(): void {
-                $.ajax({
-                    type: "GET",
-                    url: "php/DecideTheDestiney.php"
-                }).done(function (res, text, xhr) {
-                    var result = xhr.responseText.split(',')[0];
-                    var description = xhr.responseText.split(',')[1];
-                    let count = 0;
-                    let delay = 100;
-                    let interval, interval2, interval3, interval4;
-
-                    var lunch = [];
-
-                    $.getJSON("data/lunch.json", function (data) {
-                        data.forEach(function (value) {
-                            lunch.push(value.name);
-                        });
-                    });
-
-                    let roulette = function() {
-                        var moment = lunch[Math.floor(Math.random() * lunch.length)];
-
-                        let resultObj = $(".result");
-                        resultObj.html(moment);
-                        count++;
-                        if (count === 20) {
-                            clearTimeout(interval);
-                            interval2 = setInterval(roulette, 200);
-                        }
-
-                        if (count === 27) {
-                            clearTimeout(interval2);
-                            interval3 = setInterval(roulette, 800);
-                        }
-
-                        if (count === 29) {
-                            clearTimeout(interval3);
-                            interval4 = setInterval(roulette, 1200);
-                        }
-
-                        if (count === 31) {
-                            clearTimeout(interval4);
-                            $(".decision").removeClass("hidden");
-                            resultObj.html(result + '<span style="display: block; font-size: medium">' + description + '</span>');
-                            socket.emit("client_to_server", {
-                                value: result,
-                                time: formatExecTime()
-                            });
-                        }
-                    };
-                    // ルーレットスタート
-                    interval = setInterval(roulette, delay);
-                }).fail(function (res, text, xhr) {
-                    console.log(res, text, xhr);
+            start(): any {
+                doGacha().then((shops: any) =>{
+                    this.list = shops[0];
+                    this.result = shops[1];
                 });
             }
+        },
+        computed: {
         }
     });
 </script>
