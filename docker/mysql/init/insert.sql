eb00380de25034d7fbcab86f96634f277756c33c
originbenntou8973 <originbenntou8973@gmail.com>
2019-01-30 02:07:14 +0900

    UPDATE実装

--- docker/mysql/init/insert.sql
+++ docker/mysql/init/insert.sql
@@ -1,5 +1,5 @@
 use elunch
 
 INSERT INTO original VALUES
-(1, 'あじゃ', 'あじゃりずむ', 1000, 100, 5, 7, 'http://livedoor.blogimg.jp/taro_abcd-ugikkq6i/imgs/f/3/f3978605-s.jpg', 'https://zombielandsaga.com/', '〒848-0021 佐賀県伊万里市大坪町甲１３８４−２', 33.8820298, 130.8816532, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(2, 'あじゃあじゃ', 'あじゃあじゃりずむ', 2000, 200, 3, 6, 'http://livedoor.blogimg.jp/taro_abcd-ugikkq6i/imgs/f/3/f3978605-s.jpg', 'https://zombielandsaga.com/', '〒848-0021 佐賀県伊万里市大坪町甲１３８４−２', 33.8820298, 130.8816532, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+(1, 'あじゃ', 'あじゃりずむ', 100, 1000, 5, 7, 'http://livedoor.blogimg.jp/taro_abcd-ugikkq6i/imgs/f/3/f3978605-s.jpg', 'https://zombielandsaga.com/', '〒848-0021 佐賀県伊万里市大坪町甲１３８４−２', 33.8820298, 130.8816532, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, 'あじゃあじゃ', 'あじゃあじゃりずむ', 200, 2000, 3, 6, 'http://livedoor.blogimg.jp/taro_abcd-ugikkq6i/imgs/f/3/f3978605-s.jpg', 'https://zombielandsaga.com/', '〒848-0021 佐賀県伊万里市大坪町甲１３８４−２', 33.8820298, 130.8816532, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
